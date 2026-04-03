c6901a1703fd676c46e6400142d38444c2406e5f
Ashish <ashish061104@gmail.com>
2026-04-03 17:54:51 +0530

    feat(bottomdeck): updated safeareaview

--- Orbit/app/(tabs)/index.tsx
+++ Orbit/app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import "@/global.css"
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
- 
+
+import {styled} from "nativewind";
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
       <Text className="text-xl font-bold text-red-500">
         Welcome to Nativewind!
       </Text>
