e027619c35de3e97e7a92b01ef1b95ce456584c6
Ashish <ashish061104@gmail.com>
2026-04-03 23:10:32 +0530

    feat(auth): set-up

--- Orbit/app/(tabs)/index.tsx
+++ Orbit/app/(tabs)/index.tsx
@@ -2,22 +2,35 @@ import "@/global.css"
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-import {styled} from "nativewind";
-import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/subscriptions/[id]">hi</Link>
-      <Link href={{
-        pathname: "/subscriptions/[id]",
-        params: {id: "claude"},
-      }}
-      >claude</Link>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
+
+      <Link
+        href="/onboarding"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Onboarding
+      </Link>
+
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Sign in
+      </Link>
+
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
+      >
+        Go to Sign up
+      </Link>
     </SafeAreaView>
-   );
+  );
 }
